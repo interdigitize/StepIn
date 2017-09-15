a9b74e0dfd0c4a53ec86f1617868a668872ff569
interdigitize <interdigitize@users.noreply.github.com>
2017-09-15 12:39:29 -0700

    Removes db reference.

--- server/index.js
+++ server/index.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var items = require('../db');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
-
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
